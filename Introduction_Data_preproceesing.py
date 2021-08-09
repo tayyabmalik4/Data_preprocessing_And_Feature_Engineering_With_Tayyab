fc3ded3001580aada55c25532b564fb50198f2ab
tayyabmalik4 <mtayyabmalik99@gmail.com>
2021-08-10 01:04:19 +0500

    1.0

--- Introduction_Data_preproceesing.py
+++ Introduction_Data_preproceesing.py
@@ -1,1 +1,79 @@
 # *********************************Introduction of Data Preprocessing using python************************************
+
+# //////purpose of this tutorial(Introduction)
+# ----(1)-What is Data Preprocessing?
+# ----(2)-Why is Data preprocessing?
+# ----(3)-Techniques of Data Preprocessing
+# ----(4)-Data Cleaning
+# ----(5)-Data Integration
+# ----(6)-Data Reduction
+# ----(7)-Data Transformation
+# ----(8)-Data Discretization
+# ----(9)-Prerequisties
+
+
+# ***********(1)-What is Data Preprocessing?
+# -----Data-----Definition------In computing, data is information that has been translated into a form that is efficient for movement or processing.
+# -----Data-----Data is anything like(Text,images,vedios,audios etc).
+# -------Definition of Data Preprocessing----Data Preprocessing is a process to convert raw data into meaningful data using different techniques.
+
+
+# *********(2)-Why is Data Processing Importance?
+# ----Achully Data in the real world is raw-data(dirty-data)
+# ----Clearing Garbig Values----we cleaning the raw-data to meaning-full data for using to machine leaning we clean the garbeg like--- (Incomplete Data, Noisy,Inconsistend(reverse),Duplicate)
+# ----converting Quality Data-----we converting the raw-data to meaning-ful data like ---(Accuracy,Completeness,Consistency,Believability,Interpretability)
+# -----Build-Machine-Learning-Model-----Machine Learning Algorithms follow the rule (learn like kids)
+# -----quit(GIGO----Garbage In Garbage out)
+
+
+# ***********Steps to Increase the quality of data and converding the row-Data to meaning-ful Data
+# ----Data Cleaning
+# ----Data Integration
+# ----Data Reduction
+# ----Data Tranformation
+# ----Data Discretization 
+
+# ********Data Cleaning
+# ----Definition---Data Cleaning means fill in missing Values,smooth, out noise while identifying outliers and correct inconsistancies in the data
+
+# ******Data Integration
+# ----Definition---Data Integration is a technique to merges data from multiple sources into a coherent data store, such as a data warehouse.
+
+# ******Data Reduction
+# ----Definition---Data Reduction is a technique use to reduce the data size by aggregating, eliminating redundant features, or clustering for instance.
+
+# ******Data Transformation
+# /////we convered the data is one formed to different many others formated like converting to pieces
+# ----Definition---Data Transformation means data are transformed or consolidated into forms appropriate for ML model training, such as normalization, may be applied where dara are scaled to fall within a smaller range likem -1.0 to 1.0
+# ////we transform the data using these functions-----(Aggregation, Feature type conversion, Normalization, Attribute/feature construction)
+
+# ******Data Discretization
+# ----Definition---Data Discretization technique transforms numeric data by mapping values to interval or concept labels.
+# ----It can be used to reduce the number of values for a given continuous attribute by dividing the range of the attribute into intervals
+# ----Discretization techniques include---(Bining, Histogram analysis, Cluster analysis, Decision-tree analysis, Correlation analysis)
+# ----Example----Age(1,2,3,4,5,6,7,8,9) dividing and pieces (1-3,4-6, 7-9)
+
+
+# *****************Data preprocessing lies these libraies and study others
+# /////libraies
+# ---(1)-numpy
+# ---(2)-pandas
+# ---(3)-matplotlib
+# ---(4)-seaborn
+# ---(5)-scikit-learn
+
+# /////study and knowledge
+# ----(1)---Mathematics
+# ----(2)---Statistics
+# ----(3)---Probability
+# ----(4)---Calculus
+# ----(5)---Linear Algebra
+
+
+# ************Software use
+# ----Anaconda---jupyter & Spyder
+# ----but we use VS Code IDE for data Preprocessing
+# -----we use Visual Studio(VS) code IDE(Integrated Development Environment) because we upload the code in the github and jupyter notebok file is not showing in the github thats why we use VS code
+
+
+
