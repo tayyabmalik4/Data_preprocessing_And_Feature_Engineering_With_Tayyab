14402f8f9c292dd3cdb1a6feeeefcd15913b1c64
tayyabmalik4 <mtayyabmalik99@gmail.com>
2021-08-10 21:15:23 +0500

    1.0

---
+++ 06_Date_Time_And_Mixed_variables.py
@@ -0,0 +1,16 @@
+# ***************************Date & Time And also Mixed Variables in Data Preprocessing And Feature Engineering************************************
+
+# ----Purpose----
+# ---(1)-What are Date & Time Variables?
+# ---(2)-Examples
+# ---(3)-What are Mixed variables?
+# ---(4)-Examples
+
+
+# *********What are Date And Time variables?
+# ----Definition---DateTime variables can contain date only,time only or date and time.
+#  ----Examples---Date only(jan-2010,may-2020,sep-2013,oct-2012),Time-only(10:15 AM,05:00 PM,19:40 AM,15:09 PM) and DateTime(07-Dec-2020 12:00:00, 10-jan-2020 23:04:02,17-Apr-2021 12:22:14,10-Agu-2021 15:30:00, 15-sep-2021 11:22:00)
+
+# *********Mixed Variable
+# ----Definition---The variables which contains numbers and categories(labels) called as Mixed Variable 
+# ----Examples---Percentage(A++,85,Failed,Failed,74,48)
