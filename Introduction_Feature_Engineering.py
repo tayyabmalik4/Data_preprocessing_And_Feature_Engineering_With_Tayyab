11ebd58f3b45f6e3f4229163128a4bc9140f5c18
tayyabmalik4 <mtayyabmalik99@gmail.com>
2021-08-10 14:59:01 +0500

    1.0

--- Introduction_Feature_Engineering.py
+++ Introduction_Feature_Engineering.py
@@ -1,1 +1,38 @@
-# ******************************Introduction****************** 
+# ******************************Introduction of Feature Engineering using Python Libraries****************** 
+
+# ------purpose-----
+# ----(1)-What is Data Feature Engineering?
+# ----(2)-Why is Featrue Engineering Importance?
+# ----(3)-Process
+# ----(5)-Prerequisties
+
+
+# ********What is Feature Engineering?
+# ----Feature-Definition---Feature is an attribute or property shaered by all of the independent units on which analysis or prediction is to be done
+# ----Engineering-Definition---Engineering is the process to solve the problems and creating new inventing something.
+# ----Defintion of Feature Engineering--- Feature Engineeing is the process to creat feature/extract the feature from existing feactures by domain knowledge to increase the performance of Machine Learning Model.
+# ----Feature Engineering is an Art---
+
+# ******Why is Feature Engineering Importance?
+# ------Quality data always help to improve the accuracy and performance of Machine Learning model.
+# -----Machine Learning algorithm follow the rule (learn like kids)
+# ....GIGO----(garbage in  garbage out)
+
+# ------Data---->>>>Feature Engineering----->>>>>Machine Learning Model 
+
+# ********Major Process of Feature Engineering:
+# ---Brainstorming or testing features
+# ---Deciding what features to creat 
+# ---Creating features
+# ---Checking how the features work with your model
+# ---Improving your features if needed
+# ---Go back to brainstorming/Creating more features until the work is done 
+
+
+# *******Prerequisites
+# ////Python Libraries
+# ---(1)-Numpy 
+# ---(2)-pandas
+# ---(3)-matplotlib
+# ---(4)-Seaborn
+# ---(5)-scikit learn 
