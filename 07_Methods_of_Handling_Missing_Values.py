1a85f081f7da1395ff668900d57e20a7ab96e40b
tayyabmalik4 <mtayyabmalik99@gmail.com>
2021-08-11 09:32:13 +0500

    1.0

---
+++ 07_Methods_of_Handling_Missing_Values.py
@@ -0,0 +1,19 @@
+# (07)***************Methods Of Handling Missing Values in Data Processing and Feature Engineering*************************
+
+
+# **********purpose---
+# ---(1)-What is missing values?
+# ---(2)-Methods to Handling Missing Values
+
+
+# ---What is Missing values?
+# ---Definition---In the variable or any observation,values are not stored called as missing values/data
+
+
+# *******What are the Methods to Handle Missing Values?
+# ----(1)-Ignore missing values row / Delete row
+# ----(2)-Fill missing values mamnually
+# ----(3)-global constant
+# ----(4)-Measure of central tendency(Mean,Midian and Mode)
+# ----(5)-Measure of central tendency for each class
+# ----(6)-Most probable value (ML Algorithms)---Linear Regg,Decision Tree----costly method 
